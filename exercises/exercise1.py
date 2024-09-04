d3b7d8368784fed921b884343b2ce9021291e425
Tosuma <45211504+Tosuma@users.noreply.github.com>
2024-09-04 14:08:58 +0200

    Made solution to exercise 1

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -23,10 +23,31 @@ class Gossip(Device):
         self._secrets = set([index])
 
     def run(self):
-        # the following is your termination condition, but where should it be placed?
-        # if len(self._secrets) == self.number_of_devices():
-        #    return
-        return
+        next_to_call: int = (self.index() + 1) % self.number_of_devices()
+        while True:
+            if len(self._secrets) == self.number_of_devices():
+                for i in range(0, self.number_of_devices()):
+                    if i == self.index: continue
+                    self.medium().send(GossipMessage(self.index(), i, self._secrets))
+                print(f'Thread {self.index()} returns')
+                return
+            
+            if next_to_call == self.index():
+                next_to_call = (next_to_call + 1) % self.number_of_devices()
+                continue
+
+            self.medium().send(GossipMessage(self.index(), next_to_call, self._secrets))
+        
+            ingoing = self.medium().receive_all()
+            if ingoing is None:
+                continue
+            else:
+                for received in ingoing:
+                    self._secrets.update(received.secrets)
+
+            next_to_call = (next_to_call + 1) % self.number_of_devices()
+            self.medium().wait_for_next_round()
 
     def print_result(self):
         print(f'\tDevice {self.index()} got secrets: {self._secrets}')
+    
